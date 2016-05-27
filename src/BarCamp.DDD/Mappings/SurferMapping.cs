c030175912116430d0d2e0a886f436daf9e4ad50
Mickaël Derriey <mderriey@gmail.com>
2016-05-27 21:43:34 +1000

    initial commit

---
+++ src/BarCamp.DDD/Mappings/SurferMapping.cs
@@ -0,0 +1,35 @@
+﻿namespace BarCamp.DDD.Mappings
+{
+    using Models;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.ModelConfiguration;
+
+    internal class SurferMapping : EntityTypeConfiguration<Surfer>
+    {
+        public SurferMapping()
+        {
+            HasKey(x => x.Id);
+
+            Property(x => x.Id)
+                .HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+
+            Property(x => x.FirstName)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            Property(x => x.LastName)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            Property(x => x.CreatedOn)
+                .IsRequired();
+
+            Property(x => x.NameLastModifiedOn)
+                .IsRequired();
+
+            HasMany(x => x.Surfboards)
+                .WithRequired()
+                .HasForeignKey(x => x.SurferId);
+        }
+    }
+}
---
+++ src/BarCamp.DDD/Models/Surfboard.cs
@@ -0,0 +1,16 @@
+﻿namespace BarCamp.DDD.Models
+{
+    public class Surfboard
+    {
+        public int Id { get; set; }
+        public int SurferId { get; set; }
+        public string ShaperName { get; set; }
+        public int Feet { get; set; }
+        public int Inches { get; set; }
+
+        public override string ToString()
+        {
+            return $"{ShaperName} {Feet}'{Inches}\"";
+        }
+    }
+}
