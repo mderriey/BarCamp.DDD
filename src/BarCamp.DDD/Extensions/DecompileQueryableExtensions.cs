436b15dc49ba8a82f1af912ab530f0ee3a4c3897
Mickaël Derriey <mderriey@gmail.com>
2016-05-27 23:20:40 +1000

    prevent casting the IEnumerable back to an ICollection

--- src/BarCamp.DDD/Extensions/DecompileQueryableExtensions.cs
+++ src/BarCamp.DDD/Extensions/DecompileQueryableExtensions.cs
@@ -6,13 +6,18 @@
     using System.Data.Entity;
     using System.Linq;
     using System.Linq.Expressions;
+    using System.Reflection;
 
     internal static class DecompileQueryableExtensions
     {
         public static IQueryable<T> Include<T, TEnumerable>(this IQueryable<T> queryable, Expression<Func<T, IEnumerable<TEnumerable>>> path)
         {
-            var decompiled = (Expression<Func<T, IEnumerable<TEnumerable>>>)DecompileExpressionVisitor.Decompile(path);
-            return QueryableExtensions.Include(queryable, decompiled);
+            var body = path.Body;
+            var member = (PropertyInfo)((MemberExpression)body).Member;
+            var decompiled = DecompileExpressionVisitor.Decompile(body) as MethodCallExpression;
+            var converted = Expression.Convert(decompiled.Arguments[0], typeof(ICollection<TEnumerable>));
+            var collectionExpression = Expression.Lambda<Func<T, ICollection<TEnumerable>>>(converted, path.Parameters);
+            return QueryableExtensions.Include(queryable, collectionExpression);
         }
     }
 }
